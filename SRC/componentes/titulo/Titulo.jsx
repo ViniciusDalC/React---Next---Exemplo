64f319ecf5430937188051fdaa4999369a0593dc
Vinicius Dalvi Cola <vinciusdalvicola@gmail.com>
2022-12-16 14:42:08 -0300

    Criado primeiro componente React.jsx

---
+++ SRC/componentes/titulo/Titulo.jsx
@@ -0,0 +1,9 @@
+function Titulo () {
+  return (
+    <div>
+      <h1>Meu Titulo</h1>
+    </div>
+  )
+}
+
+export default Titulo
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,11 @@
+import Titulo from '../SRC/componentes/titulo/Titulo'
+
 function HomePage () {
   return (
     <div>
+      <Titulo/>
+      <Titulo/>
+      <Titulo/>
       Hello Word, NextJs + ReactJs
     </div>
   )
